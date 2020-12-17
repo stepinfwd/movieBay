aec8612fdab911ec0ffbf753cb837410119c5a98
kiran <kiran@dexlock.com>
2020-12-17 18:09:21 +0530

    gallery redux bug fix

--- src/components/Redux/Reducers/GalleryReducer.js
+++ src/components/Redux/Reducers/GalleryReducer.js
@@ -10,7 +10,7 @@ export default function galleryRedcuer(state = initialState, action) {
     case "fetch_gallerys_request":
       return { ...state, loading: true };
     case "fetch_gallerys_success":
-      return { ...state, gallerys: action.payload, loading: false };
+      return { ...state, galleryImg: action.payload, loading: false };
     case "fetch_gallerys_failure":
       return { ...state, error: action.payload, loading: false };
     default:
